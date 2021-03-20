43f1439ad05627a3861982782292c3f29e23a978
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-20 20:11:44 +0000

    added click event for start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("working");
+const startQuizBtn = document.getElementById("start-btn");
+const introDivElement = document.getElementById("intro-section");
+const bodyElement = document.body;
+
+const startQuiz = () => {
+  // removed intro div
+  bodyElement.removeChild(introDivElement);
+
+  // start timer
+};
+
+startQuizBtn.addEventListener("click", startQuiz);
