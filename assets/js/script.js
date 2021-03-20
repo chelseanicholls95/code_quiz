02e0521dd64576185ec75f842d7d168cd55e00cd
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-20 20:22:08 +0000

    added start timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
+const timerSpanElement = document.getElementById("timer");
 const startQuizBtn = document.getElementById("start-btn");
 const introDivElement = document.getElementById("intro-section");
 const bodyElement = document.body;
 
+let timerValue = 10;
+
+const startTimer = () => {
+  const timerTick = () => {
+    timerSpanElement.textContent = timerValue;
+    timerValue -= 1;
+
+    if (timerValue < 0) {
+      clearInterval(timer);
+    }
+  };
+
+  const timer = setInterval(timerTick, 1000);
+};
+
 const startQuiz = () => {
   // removed intro div
   bodyElement.removeChild(introDivElement);
-
   // start timer
+  startTimer();
 };
 
 startQuizBtn.addEventListener("click", startQuiz);
