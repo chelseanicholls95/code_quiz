c022ae741232688182593638e8bccb6e2c39c282
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-20 22:02:36 +0000

    added event listener to submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,58 @@ const timerSpanElement = document.getElementById("timer");
 const startQuizBtn = document.getElementById("start-btn");
 const introDivElement = document.getElementById("intro-section");
 const bodyElement = document.body;
+const scriptElement = document.getElementById("script");
 
 let timerValue = 3;
+
+const submitScore = (event) => {
+  event.preventDefault();
+  console.log("submitted");
+};
+
+const createAndAppendForm = () => {
+  // create elements for all html elements
+  const sectionElement = document.createElement("section");
+  const divElement = document.createElement("div");
+  const h2Element = document.createElement("h2");
+  const infoDivElement = document.createElement("div");
+  const spanElement = document.createElement("span");
+  const formElement = document.createElement("form");
+  const inputElement = document.createElement("input");
+  const submitScoreBtn = document.createElement("button");
+
+  // set attributes for html elements
+  sectionElement.setAttribute("class", "section");
+  divElement.setAttribute("class", "section-div");
+  h2Element.setAttribute("class", "titles");
+  infoDivElement.setAttribute("class", "info");
+  formElement.setAttribute("id", "score-form");
+  inputElement.setAttribute("type", "text");
+  inputElement.setAttribute("placeholder", "Enter your initials here");
+  inputElement.setAttribute("id", "name-input");
+  submitScoreBtn.setAttribute("class", "submit-btn");
+  submitScoreBtn.setAttribute("id", "submit-btn");
+  submitScoreBtn.setAttribute("type", "submit");
+
+  // add content to html elements
+  h2Element.textContent = "Game Over";
+  infoDivElement.textContent = "Your score is: ";
+  spanElement.textContent = "0";
+  submitScoreBtn.textContent = "Submit";
+
+  // add event listener to button
+  formElement.addEventListener("submit", submitScore);
+
+  // add html elements to the parent elements
+  bodyElement.insertBefore(sectionElement, scriptElement);
+  sectionElement.appendChild(divElement);
+  divElement.appendChild(h2Element);
+  divElement.appendChild(infoDivElement);
+  infoDivElement.appendChild(spanElement);
+  divElement.appendChild(formElement);
+  formElement.appendChild(inputElement);
+  formElement.appendChild(submitScoreBtn);
+};
 
 const startTimer = () => {
   const timerTick = () => {
@@ -17,39 +67,6 @@ const startTimer = () => {
   };
 
   const timer = setInterval(timerTick, 1000);
-};
-
-const createAndAppendForm = () => {
-  const sectionElement = document.createElement("section");
-  const divElement = document.createElement("div");
-  const h2Element = document.createElement("h2");
-  const infoDivElement = document.createElement("div");
-  const spanElement = document.createElement("span");
-  const formElement = document.createElement("form");
-  const inputElement = document.createElement("input");
-  const submitBtn = document.createElement("button");
-
-  sectionElement.setAttribute("class", "section");
-  divElement.setAttribute("class", "section-div");
-  h2Element.setAttribute("class", "titles");
-  infoDivElement.setAttribute("class", "info");
-  inputElement.setAttribute("type", "text");
-  inputElement.setAttribute("placeholder", "Enter your initials here");
-  submitBtn.setAttribute("class", "submit-btn");
-
-  h2Element.textContent = "Game Over";
-  infoDivElement.textContent = "Your score is: ";
-  spanElement.textContent = "0";
-  submitBtn.textContent = "Submit";
-
-  bodyElement.appendChild(sectionElement);
-  sectionElement.appendChild(divElement);
-  divElement.appendChild(h2Element);
-  divElement.appendChild(infoDivElement);
-  infoDivElement.appendChild(spanElement);
-  divElement.appendChild(formElement);
-  formElement.appendChild(inputElement);
-  formElement.appendChild(submitBtn);
 };
 
 const startQuiz = () => {
