d791980d048bb4e4a14b1818635b674bcebafb46
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-27 19:53:01 +0000

    added comments and final amendments

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -4,8 +4,10 @@ const clearBtn = document.getElementById("clear-btn");
 index = 0;
 
 const getHighScores = () => {
+  // get high scores from local storage
   const highScores = localStorage.getItem("highScores");
 
+  // if there is high scores data saved in local storage - convert the string to an object and return. If no data - return empty array
   if (highScores) {
     return JSON.parse(highScores);
   } else {
@@ -13,28 +15,32 @@ const getHighScores = () => {
   }
 };
 
+// sort scores highest to lowest
 const sortHighScores = (lowestScore, highestScore) =>
   highestScore.score - lowestScore.score;
 
 const createTable = (highScores) => {
+  const createRow = (highScore) => {
+    // create row, set attributes and text content
+    const row = table.insertRow(-1);
+    row.setAttribute("class", "table-row");
+    row.insertCell(-1).textContent = highScore.initials;
+    row.insertCell(-1).textContent = highScore.score;
+    index += 1;
+  };
+  // create table and set attributes
   const table = document.createElement("table");
   table.setAttribute("class", "table");
   table.setAttribute("id", "table");
 
-  const createRow = (highScores) => {
-    const row = table.insertRow(-1);
-    row.setAttribute("class", "table-row");
-    row.insertCell(-1).textContent = highScores.initials;
-    row.insertCell(-1).textContent = highScores.score;
-    index += 1;
-  };
-
   highScores.forEach(createRow);
 
+  // append table to high scores div
   highScoresDiv.appendChild(table);
 };
 
 const renderHighScoresTable = (highScores) => {
+  // if high scores has no content - show 'no high scores saved', otherwise create high scores table
   if (highScores.length === 0) {
     highScoresDiv.textContent = "No high scores saved";
   } else {
@@ -43,17 +49,18 @@ const renderHighScoresTable = (highScores) => {
 };
 
 const clear = () => {
+  // clear local storage, remove table and show 'no high scores saved'
   localStorage.clear();
   highScoresDiv.removeChild(document.getElementById("table"));
   highScoresDiv.textContent = "No high scores saved";
 };
 
 const onLoad = () => {
+  // get high scores, sort them and render high scores table
   const highScores = getHighScores();
   const orderedHighScores = highScores.sort(sortHighScores);
   renderHighScoresTable(orderedHighScores);
 };
 
 clearBtn.addEventListener("click", clear);
-
 window.addEventListener("load", onLoad);
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ const scriptElement = document.getElementById("script");
 
 let timerValue = 60;
 let index = 0;
-timerSpanElement.textContent = timerValue;
 
 const questionsArray = [
   {
@@ -44,8 +43,10 @@ const questionsArray = [
 ];
 
 const getHighScores = () => {
+  // get high scores from local storage
   const highScores = localStorage.getItem("highScores");
 
+  // if there is high scores data saved in local storage - convert the string to an object and return. If no data - return empty array
   if (highScores) {
     return JSON.parse(highScores);
   } else {
@@ -74,42 +75,49 @@ const submitScore = (event) => {
       highScores.push(highScore);
       localStorage.setItem("highScores", JSON.stringify(highScores));
     }
-
+    // go to high scores page
     location.href =
       "https://chelseanicholls95.github.io/code_quiz/highscores.html";
   }
 };
 
 const createAndAppendForm = () => {
+  // create and append div
   const divElement = document.createElement("div");
   divElement.setAttribute("class", "section-div");
   quizContainer.appendChild(divElement);
 
+  // create and append h2
   const h2Element = document.createElement("h2");
   h2Element.setAttribute("class", "title");
   h2Element.textContent = "Game Over";
   divElement.appendChild(h2Element);
 
+  // create and append info div
   const infoDivElement = document.createElement("div");
   infoDivElement.setAttribute("class", "info");
   infoDivElement.textContent = "Your score is: ";
   divElement.appendChild(infoDivElement);
 
+  // create and append span to show score
   const spanElement = document.createElement("span");
   spanElement.textContent = timerValue;
   infoDivElement.appendChild(spanElement);
 
+  // create and append form
   const formElement = document.createElement("form");
   formElement.setAttribute("id", "score-form");
   formElement.addEventListener("click", submitScore);
   divElement.appendChild(formElement);
 
+  // create and append initials input
   const inputElement = document.createElement("input");
   inputElement.setAttribute("type", "text");
   inputElement.setAttribute("placeholder", "Enter your initials here");
   inputElement.setAttribute("id", "name-input");
   formElement.appendChild(inputElement);
 
+  // create and append submit button
   const submitScoreBtn = document.createElement("button");
   submitScoreBtn.setAttribute("class", "btn submit-btn");
   submitScoreBtn.setAttribute("id", "submit-btn");
@@ -119,21 +127,29 @@ const createAndAppendForm = () => {
 };
 
 const showGameOverContainer = () => {
+  // remove quiz container and create form
   quizContainer.removeChild(document.getElementById("question-container"));
   createAndAppendForm();
 };
 
 const startTimer = () => {
-  const timerTick = () => {
-    timerSpanElement.textContent = timerValue;
-    timerValue -= 1;
+  // set timer to starting value
+  timerSpanElement.textContent = timerValue;
 
+  const timerTick = () => {
+    // remove one from the timer and set to timer
+    timerValue -= 1;
+    timerSpanElement.textContent = timerValue;
+
+    // if the timer is less than or equal to 0 - set to 0 and show game over container
     if (timerValue <= 0) {
       timerValue = 0;
       timerSpanElement.textContent = timerValue;
       clearInterval(timer);
       showGameOverContainer();
     }
+
+    // if all questions in the array have been shown - stop timer and show game over container
     if (index === questionsArray.length) {
       clearInterval(timer);
       showGameOverContainer();
@@ -148,45 +164,64 @@ const createAndAppendChoices = (choices) => {
   const parentDiv = document.createElement("div");
 
   createChoice = (choice) => {
+    // create div and button
     const divElement = document.createElement("div");
-
     const button = document.createElement("button");
+
+    // set attributes and text content
     button.setAttribute("data-answer", choice);
     button.setAttribute("class", "btn choice-btn");
     button.textContent = choice;
 
+    // append
     divElement.appendChild(button);
-
     parentDiv.appendChild(divElement);
   };
-  choices.forEach(createChoice);
 
+  choices.forEach(createChoice);
   return parentDiv;
 };
 
 const createQuestion = (question) => {
+  // create div and set attributes
   const divElement = document.createElement("div");
   divElement.setAttribute("id", "question-container");
   divElement.setAttribute("class", "section-div");
   divElement.setAttribute("data-answer", question.answer);
 
+  // create h2 and set attributes
   const h2Element = document.createElement("h2");
   h2Element.setAttribute("class", "question");
   h2Element.textContent = question.question;
 
+  // create choice buttons
   const choices = createAndAppendChoices(question.choices);
 
+  // add event listener to div
   divElement.addEventListener("click", checkAnswer);
 
+  // append h2 and choices to div
   divElement.append(h2Element, choices);
 
   return divElement;
 };
 
 const renderQuestion = (question) => {
+  // if there are questions left in the questions array - create a question card and append to the quiz container
   if (index < questionsArray.length) {
     const questionContainer = createQuestion(question);
     quizContainer.appendChild(questionContainer);
+  }
+};
+
+const replaceQuestion = () => {
+  // target question container
+  const questionContainer = document.getElementById("question-container");
+
+  // if there are questions left in the questions array - remove current question container and render the next
+  if (index < questionsArray.length) {
+    quizContainer.removeChild(questionContainer);
+    renderQuestion(questionsArray[index]);
   }
 };
 
@@ -195,19 +230,14 @@ const checkAnswer = (event) => {
   const target = event.target;
   const currentTarget = event.currentTarget;
 
+  // if the element that was clicked is a button - get users answer and correct answer
   if (target.matches("button")) {
     const answer = target.dataset.answer;
     const correctAnswer = currentTarget.getAttribute("data-answer");
 
+    // if the users answer is correct - replace question with next question
     if (answer === correctAnswer) {
-      const replaceQuestion = () => {
-        const questionContainer = document.getElementById("question-container");
-        if (index < questionsArray.length) {
-          quizContainer.removeChild(questionContainer);
-          renderQuestion(questionsArray[index]);
-        }
-      };
-
+      // increase index by 1 and set class to 'correct-answer'
       index += 1;
       target.setAttribute("class", "correct-answer");
 
@@ -225,6 +255,7 @@ const checkAnswer = (event) => {
       // time out of half a second to show correct text
       setTimeout(replaceQuestion, 500);
     } else {
+      // set class of 'wrong-answer'
       target.setAttribute("class", "wrong-answer");
 
       // remove previous div
@@ -251,8 +282,10 @@ const checkAnswer = (event) => {
 const startQuiz = () => {
   // remove intro div
   quizContainer.removeChild(introDivElement);
+
   // start timer
   startTimer();
+
   // create question div
   renderQuestion(questionsArray[index]);
 };
